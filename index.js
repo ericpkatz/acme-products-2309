52a83b829af0f434aa99ff99706faf48999fb9f5
ericpkatz <ericpkatz@gmail.com>
2023-11-30 11:40:13 -0500

    adding random freelancers

--- index.js
+++ index.js
@@ -4,25 +4,50 @@ const ul = document.querySelector('ul');
 
 const products = [
   { name: 'foo', color: 'red', cost: 1},
-  { name: 'blue', color: 'blue', cost: 3},
+  { name: 'bar', color: 'blue', cost: 3},
 ];
 
-spanCount.innerHTML = products.length;
+const names = ['foo', 'bar', 'bazz', 'quq', 'fizz', 'buzz'];
+const colors = ['red', 'blue', 'green', 'yellow'];
 
-let sum = 0;
-products.forEach((product)=> {
-  sum = sum + product.cost;
-});
-const average = sum / products.length;
-spanAverageCost.innerHTML = average;
+function render(){
+  spanCount.innerHTML = products.length;
 
-const html = products.map((product)=> {
-  return `
-    <li>
-      ${product.name} is ${product.color } and cost ${product.cost}
-    </li>
-  `;
-}).join('');
+  let sum = 0;
+  products.forEach((product)=> {
+    sum = sum + product.cost;
+  });
+  const average = sum / products.length;
+  spanAverageCost.innerHTML = average.toFixed(2);
 
-ul.innerHTML = html;
+  const html = products.map((product)=> {
+    return `
+      <li>
+        ${product.name} is ${product.color } and cost $${product.cost.toFixed(2)}
+      </li>
+    `;
+  }).join('');
+
+  ul.innerHTML = html;
+}
+
+render();
+
+const interval = setInterval(()=> {
+  const nameIndex = Math.floor(Math.random()*names.length);
+  const name = names[nameIndex];
+  const colorIndex = Math.floor(Math.random()*colors.length);
+  const color = colors[colorIndex];
+  const cost = Math.ceil(Math.random()*10);
+  const randomProduct = {
+    name: name,
+    color: color,
+    cost: cost
+  };
+  products.push(randomProduct);
+  render();
+  if(products.length === 15){
+    clearInterval(interval);
+  }
+}, 1000)
 
